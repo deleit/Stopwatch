358fb2fc7d4a67a0de3a032b73e943f0d0f846a6
Deivison Leite <dlmacedo9@gmail.com>
2021-09-29 02:15:12 -0300

    Obtendo a opção

--- Program.cs
+++ Program.cs
@@ -20,6 +20,8 @@ namespace Stopwatch
 
             string data = Console.ReadLine().ToLower();
             char type = char.Parse(data.Substring(data.Length - 1, 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+
         }
 
         static void Start(int time)
